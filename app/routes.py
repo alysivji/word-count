bbbe8d25f28723738baa11342179377003a3e3d5
Aly Sivji <alysivji@gmail.com>
2018-04-01 11:19:22 -0500

    Implement basic word count algo

--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,28 @@
+from collections import Counter
 import logging
 import os
+import string
 
-from flask import flash, render_template, url_for, redirect, request
+from flask import flash, render_template, request
 from werkzeug.utils import secure_filename
 
 from app import app
 from app.forms import WordCountForm
 
+###############
+# Configuration
+###############
+
 logger = logging.getLogger(__name__)
 
 ALLOWED_EXTENSIONS = set(['txt'])
 
+# map punctuation to None
+table = str.maketrans({key: None for key in string.punctuation})
+
 
 ##################
-# Helper Functinos
+# Helper Functions
 ##################
 
 def _allowed_file(filename):
@@ -21,11 +30,11 @@ def _allowed_file(filename):
             filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS)
 
 
-def extract_lines_from_file(file):
+def _extract_lines_from_file(file):
     lines = []
     with open(file) as f:
         for line in f.readlines():
-            lines.append(line.strip())
+            lines.append(line.strip().translate(table))
     return lines
 
 
@@ -46,25 +55,30 @@ def index():
                 filename = secure_filename(file.filename)
                 full_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
                 file.save(full_path)
-                lines = extract_lines_from_file(full_path)
-                return url_for('word_count', lines=lines)
+                lines = _extract_lines_from_file(full_path)
+                return word_count(lines=lines)
             else:
                 flash('Error with file. Requires extension txt')
 
         # user enters text
         if form.entered_text.data is not '':
             lines = []
-            for line in form.entered_text.data.split():
-                lines.append(line.strip())
-            return url_for('word_count', lines=lines)
+            for line in form.entered_text.data.splitlines():
+                lines.append(line.strip().translate(table))
+            return word_count(lines=lines)
 
     return render_template('index.html', form=form)
 
 
-@app.route('/wc')
 def word_count(lines):
     """
     Loop thru lines and count words
     """
-    from flask import jsonify  # noqa
-    return jsonify(lines)
+    # import pdb; pdb.set_trace()
+    word_counter = Counter()
+    for line in lines:
+        words = [word.lower() for word in line.split()]
+        word_counter += Counter(words)
+
+    from flask import jsonify
+    return jsonify(word_counter)
